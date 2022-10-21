a03e4c1e85913fcc033e21ffd5e6ad0aa5c25a52
f3if3i <lottie0914@outlook.com>
2022-10-21 21:12:31 +0900

    add apiendpoint

---
+++ pages/api/database.ts
@@ -0,0 +1,24 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next"
+import { Client } from "@notionhq/client"
+import { PartialDatabaseObjectResponse } from "@notionhq/client/build/src/api-endpoints"
+
+type Data = {
+  response: PartialDatabaseObjectResponse
+}
+
+const notion = new Client({
+    auth: process.env.NOTION_KEY,
+})
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    (async () => {
+        const databaseId = process.env.NOTION_DATABASE_ID 
+        const response = await notion.databases.retrieve({ database_id: databaseId as string })
+        console.log(response)
+        res.status(200).json({ response: response })
+    })()
+}
