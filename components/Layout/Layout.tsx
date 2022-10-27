fe6b4474fd04a0b6b563b60abfa0b8e10a06abcd
f3if3i <lottie0914@outlook.com>
2022-10-27 19:47:49 +0900

    change layout's css module import to css in js

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,19 +1,35 @@
 import Header from "../Header/Header"
 import Footer from "../Footer/Footer"
-import styles from "./Layout.module.css"
 import { ReactElement } from "react"
+import { css } from "@emotion/react"
 
 type LayoutProps = {
     children: ReactElement
 }
-
-
 export default function Layout({ children }: LayoutProps) {
     return (
-        <div className={styles.container}>
+        <div css={styles.container}>
             <Header />
-            <div className={styles.content}>{children}</div>
+            <div css={styles.content}>{children}</div>
             <Footer />
         </div>
     )
+}
+
+const styles = {
+    container: css({
+        backgroundColor: "#f5f5f5",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        minHeight: "800px"
+    }),
+    content: css({
+        maxWidth: "1366px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+    })
 }
