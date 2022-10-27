148b1a433a180319582748465f85a0487446a571
f3if3i <lottie0914@outlook.com>
2022-10-27 20:00:34 +0900

    change header's css module import to css in js

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,14 +1,23 @@
-import styles from "../../components/Header/Header.module.css"
-
-// type HeaderProps = {
-// 	// name: string
-// }
+import { css } from "@emotion/react"
 
 const Header = () => {
-
     return (
-        <div className={styles.container}>🚀 0.1.0 released</div>
+        <div css={styles.container}>🚀 0.1.0 released</div>
     )
+}
+
+const styles = {
+    container: css({
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: "rgb(0, 0, 0)",
+        padding: "0.3rem",
+        color: "white",
+        width: "100%",
+        textAlign: "center"
+    })
 }
 
 export default Header
