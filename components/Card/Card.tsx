02d86c0637a56d7abd1292ca7545725075b8133a
f3if3i <lottie0914@outlook.com>
2022-11-03 17:55:01 +0900

    refactor card component

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,25 +1,107 @@
 
 import { css } from "@emotion/react"
-import React, { ReactEventHandler } from "react"
+import React, { useEffect, useState } from "react"
+import database from "../../pages/api/database"
+import { User } from "../../pages/api/user"
+import { DBDataType, DBInfoType } from "../../types/database"
 
-type CardProps = {
-    title: string
-    description: string
-    ifFlipped: boolean
-    onClick: ReactEventHandler
+type FlashcardProps = {
+    databaseContent: DBDataType[],
+    databaseInfo: DBInfoType,
+    userInfo: User
 }
-const Card = ({ title, description, ifFlipped, onClick }: CardProps) => {
+const Flashcard = ({ databaseContent, databaseInfo, userInfo }: FlashcardProps) => {
+    const [name, setName] = useState("")
+    const [description, setDescription] = useState("")
+    const [ifFlipped, setIfFlipped] = useState(false)
+    const [contentIndex, setContentIndex] = useState<number>(0)
+
+    useEffect(() => {
+        if (databaseContent) {
+            setName(databaseContent[0].name)
+            setDescription(databaseContent[0].description)
+            setContentIndex(0)
+        }
+
+    }, [databaseContent])
+
+    const flipCard = () => {
+        setIfFlipped((prev) => !prev)
+    }
+
+    const setNewCard = (index: number) => {
+
+        setName(databaseContent[index].name)
+        setDescription(databaseContent[index].description)
+    }
+
+    const indexIncrement = () =>
+        setContentIndex((prev) => prev + 1)
+
+
+    const handleClick = () => {
+        flipCard()
+    }
+    const handleNextButton = () => {
+
+        if (contentIndex < databaseContent.length - 1) {
+            indexIncrement()
+            setIfFlipped(false)
+            setNewCard(contentIndex + 1)
+        } else {
+            setName("Congrats!!")
+        }
+
+    }
+
+
     return (
-        <div
-            css={styles.card}
-            onClick={onClick}
-        >
-            {ifFlipped ? <p css={styles.description}>{description}</p> : <h2 css={styles.name}>{title}</h2>}
+        <div>
+            <div css={styles.flashCardContainer}>
+
+                <div
+                    css={styles.card}
+                    onClick={handleClick}
+                >
+                    {ifFlipped ? <p css={styles.description}>{description}</p> : <h2 css={styles.name}>{name}</h2>}
+                </div>
+            </div>
+            <div css={styles.flashcardControlContainer}>
+                <div css={styles.flashCardGeneralInfo}>
+                    <p>Hi! <span css={styles.underline}>{userInfo && userInfo.userName}</span></p>
+                    <p>Your are now learning <span css={styles.underline}>{databaseInfo && databaseInfo.name}</span></p>
+
+                    <p> Progress: <span css={styles.underline}>{contentIndex + 1}</span> / {database.length}</p>
+                </div>
+                <button
+                    css={[styles.submitButton]}
+                    onClick={handleNextButton}>Next
+                </button>
+            </div>
         </div>
+
     )
 }
 
 const styles = {
+    flashCardGeneralInfo: css({
+    }),
+    flashcardControlContainer: css({
+        display: "flex",
+        flexDirection: "column",
+        fontSize: "18px",
+        fontFamily: "'Kanit', serif",
+        fontWeight: "400",
+        padding: "20px",
+    }),
+    flashCardContainer: css({
+        width: "76%",
+        height: "100%",
+        padding: "18px"
+    }),
+    underline: css({
+        textDecoration: "rgba(252, 186, 3) wavy underline"
+    }),
     card: css({
         margin: "1rem",
         padding: "4rem",
@@ -47,9 +129,27 @@ const styles = {
         fontSize: "22px",
         lineHeight: 1.5,
         textOverflow: "ellipsis"
-    })
+    }),
+    submitButton: css({
+        borderRadius: "28px",
+        color: "rgba(252, 186, 3)",
+        backgroundColor: "black",
+        padding: "6px 16px",
+        border: "solid 2px black",
+        fontSize: "16px",
+        fontFamily: "'Kanit', serif",
+        fontWeight: "600",
+        transition: "all 1s ease",
+        ":hover": {
+            borderRight: "solid 2px black",
+            borderBottom: "solid 4px black",
+        },
+        alignSelf: "center",
+        margin: "8px 28px 34px 28px",
+        width: "120px"
+    }),
 
 }
 
-export default Card
+export default Flashcard
 
