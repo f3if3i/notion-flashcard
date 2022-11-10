bbb192f05cd1b8426ecc58bfbc76a4bbcca31b85
f3if3i <lottie0914@outlook.com>
2022-11-10 19:56:43 +0900

    redux setup wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,20 @@ import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { NextPage } from "next"
 import type { ReactElement, ReactNode } from "react"
+import { Provider } from "react-redux"
+import store from "../store/store"
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode
+    getLayout?: (page: ReactElement) => ReactNode
 }
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
+    Component: NextPageWithLayout
 }
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
     // Use the layout defined at the page level, if available
     const getLayout = Component.getLayout ?? ((page) => page)
 
-    return getLayout(<Component {...pageProps} />)
+    return getLayout(<Provider store={store}><Component {...pageProps} /></Provider>)
 }
 
