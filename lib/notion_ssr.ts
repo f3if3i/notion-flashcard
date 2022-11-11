08f7db7f18cfa9abae68799b198d7299d4705f75
f3if3i <lottie0914@outlook.com>
2022-11-11 13:10:01 +0900

    remove console.log

--- lib/notion_ssr.ts
+++ lib/notion_ssr.ts
@@ -5,20 +5,16 @@ const notion = new Client({ auth: process.env.NEXT_PUBLIC_NOTION_KEY })
 
 export const queryDatabases = async (databaseId: string) => {
     const responses = await notion.databases.query({ database_id: databaseId! })
-    console.log(responses)
-    console.log("Successfully fetched!")
     return responses
 
 }
 
 export const retrieveUser = async (userId: string) => {
     const response = await notion.users.retrieve({ user_id: userId })
-    console.log(response)
     return response
 }
 
 export const retrieveDatabase = async (databaseId: string) => {
     const response = await notion.databases.retrieve({ database_id: databaseId })
-    console.log(response)
     return response
 }
