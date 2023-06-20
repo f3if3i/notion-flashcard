df4db76857317613e38bbe11a7a99af4989730b0
feifei0914kwc <yufeifei.li@kddi-web.com>
2023-06-20 14:45:11 +0900

    default and dark theme added

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,29 +1,58 @@
-export const theme = {
-    colors: {
-        secondary: { main: "rgba(252, 186, 3)" },
-        caution: { main: "#EE4521" },
-        white: "#FFFFFF",
-        black: {
-            main: "#1B1C1E",
-            dark: "#000000"
-        },
-        grey: {
-            100: "#F5F5F5",
-            200: "#EEEBE4",
-            300: "#EAEAEA",
-            400: "#B0B0B0"
-        },
-        shadow: {
-            100: "rgba(0, 0, 0, 0.18)",
-            200: "rgba(0, 0, 0, 0.8)"
-        },
-        accentShadow: {
-            100: "rgba(245, 188, 0, 0.05)",
-            200: "rgba(245, 188, 0, 0.1)",
-            300: "rgba(252, 186, 3, 0.2)",
-            400: "rgba(252, 186, 3, 0.3)",
-            500: "rgba(252, 186, 3, 0.4)"
-
+export const themes = {
+    default: {
+        colors: {
+            secondary: { main: "rgba(252, 186, 3)" },
+            caution: { main: "#EE4521" },
+            white: "#FFFFFF",
+            black: {
+                main: "#1B1C1E",
+                dark: "#000000"
+            },
+            grey: {
+                100: "#F5F5F5",
+                200: "#EEEBE4",
+                300: "#EAEAEA",
+                400: "#B0B0B0"
+            },
+            shadow: {
+                100: "rgba(0, 0, 0, 0.18)",
+                200: "rgba(0, 0, 0, 0.8)"
+            },
+            accentShadow: {
+                100: "rgba(245, 188, 0, 0.05)",
+                200: "rgba(245, 188, 0, 0.1)",
+                300: "rgba(252, 186, 3, 0.2)",
+                400: "rgba(252, 186, 3, 0.3)",
+                500: "rgba(252, 186, 3, 0.4)"
+            }
+        }
+    },
+    dark: {
+        colors: {
+            secondary: { main: "#EE4521" },
+            caution: { main: "#EE4521" },
+            white: "#FFFFFF",
+            black: {
+                main: "#1B1C1E",
+                dark: "#000000"
+            },
+            grey: {
+                100: "#F5F5F5",
+                200: "#EEEBE4",
+                300: "#EAEAEA",
+                400: "#B0B0B0"
+            },
+            shadow: {
+                100: "rgba(0, 0, 0, 0.18)",
+                200: "rgba(0, 0, 0, 0.8)"
+            },
+            accentShadow: {
+                100: "rgba(245, 188, 0, 0.05)",
+                200: "rgba(245, 188, 0, 0.1)",
+                300: "rgba(252, 186, 3, 0.2)",
+                400: "rgba(252, 186, 3, 0.3)",
+                500: "rgba(252, 186, 3, 0.4)"
+            }
         }
     }
 }
