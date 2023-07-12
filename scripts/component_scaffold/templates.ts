3e3244ae4bd71c56e87fa6b6e1295a13fa010140
feifei0914kwc <yufeifei.li@kddi-web.com>
2023-07-12 12:05:21 +0900

    fix scaffold template

--- scripts/component_scaffold/templates.ts
+++ scripts/component_scaffold/templates.ts
@@ -1,20 +1,21 @@
 export const createComponentTemplate = (
     name: string,
 ): string => `import { css, useTheme } from "@emotion/react"
+import { Theme } from "../../../styles/theme"
 
 type ${name}Props = {
 	name: string
 }
 
 const ${name} = ({ name }: ${name}Props) => {
-    const theme = useTheme()
+    const theme = useTheme() as Theme
     const styles = getStyles(theme)
     return (
         <div css={styles.container}>{name} Sample</div>
     )
 }
 
-const getStyles = (theme: any) => {
+const getStyles = (theme: Theme) => {
     return ({
         container: css({
             backgroundColor: "pink"
