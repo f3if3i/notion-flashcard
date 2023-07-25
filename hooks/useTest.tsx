3fceb53746bd73b18896a37f8086fed40a92ac8c
feifei0914kwc <yufeifei.li@kddi-web.com>
2023-07-25 13:50:51 +0900

    useTest state fix

--- hooks/useTest.tsx
+++ hooks/useTest.tsx
@@ -1,19 +1,28 @@
 import { useSelector } from "react-redux"
 import { selectDatabase } from "../store/databaseSlice"
-import { selectUser } from "../store/userSlice"
 import { getTestElements } from "../utils/array"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+
+type testArrType = {
+    name: string
+    options: string[]
+    description: string
+}
 
 export const useTest = () => {
     const database = useSelector(selectDatabase)
     const databaseContent = database.contents
 
-    const user = useSelector(selectUser)
     const [score, setStore] = useState<number>(0)
+    const [testArr, setTestArr] = useState<testArrType[]>([])
     const testLength = 20
     const optionLength = 4
-    const testArray = getTestElements(databaseContent, testLength, optionLength)
-    const [testArr, setTestArr] = useState(testArray)
+    useEffect(() => {
+        if (databaseContent) {
+            const testArray = getTestElements(databaseContent, testLength, optionLength)
+            setTestArr(testArray)
+        }
+    }, [databaseContent])
 
-    return { testArr, setTestArr, score, setStore, user }
+    return { testArr, setTestArr, score, setStore }
 }
