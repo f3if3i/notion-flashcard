2c8408e4435a0428c75e2b25b41c27d4d1c25c21
feifei0914kwc <yufeifei.li@kddi-web.com>
2023-08-08 14:48:35 +0900

    add h5 h6 variant to Typography

--- components/atoms/Typography/Typography.tsx
+++ components/atoms/Typography/Typography.tsx
@@ -1,20 +1,21 @@
 import { css, useTheme } from "@emotion/react"
 import { Theme } from "../../../styles/theme"
 
-type TypographyVariantType = "body1" | "body2" | "h4"
+type TypographyVariantType = "body1" | "body2" | "h4" | "h5" | "h6"
 
 type TypographyProps = {
     variant: TypographyVariantType
     color?: string
     fontWeight?: string
+    customCss?: any
     children: React.ReactNode
 }
 
-const Typography = ({ variant, color, fontWeight, children }: TypographyProps) => {
+const Typography = ({ variant, color, fontWeight, customCss, children }: TypographyProps) => {
     const theme = useTheme() as Theme
     const styles = getStyles(color ?? theme.colors.black.main, fontWeight ?? "400")
     return (
-        <div css={[theme.typography[variant], styles.container]}>{children}</div>
+        <div css={[theme.typography[variant], styles.container, customCss]}>{children}</div>
     )
 }
 
@@ -22,7 +23,11 @@ const getStyles = (color: string, fontWeight: string) => {
     return ({
         container: css({
             color,
-            fontWeight
+            fontWeight,
+            whiteSpace: "nowrap",
+            // width: "80px",
+            // overflow: "hidden",
+            // textOverflow: "ellipsis",
         })
     })
 }
