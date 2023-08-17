f256b2fa37ff04ed95f8b406ba97e4e9b385b292
feifei0914kwc <yufeifei.li@kddi-web.com>
2023-08-17 18:03:17 +0900

    add testReport

--- components/organisms/TestReport/TestReport.tsx
+++ components/organisms/TestReport/TestReport.tsx
@@ -1,23 +1,55 @@
 import { css, useTheme } from "@emotion/react"
 import { Theme } from "../../../styles/theme"
+import Card from "../../atoms/Card"
+import ReportHeader from "../../molecules/ReportHeader/ReportHeader"
+import Section from "../../molecules/Section/Section"
+import { User } from "../../../pages/api/user"
+import { DBInfoType } from "../../../types/database"
+import { testItemType } from "../../../types/test"
+import QuestionResultItem from "../../molecules/QuestionResultItem/QuestionResultItem"
 
 type TestReportProps = {
-    name: string
+    testArray: testItemType[]
+    databaseInfo: DBInfoType,
+    userInfo: User,
+    selectedOptions: string[],
+    score: number
 }
 
-const TestReport = ({ name }: TestReportProps) => {
+const TestReport = ({ testArray, databaseInfo, userInfo, selectedOptions, score }: TestReportProps) => {
     const theme = useTheme() as Theme
     const styles = getStyles(theme)
     return (
-        <div css={styles.container}>{name} Sample</div>
+        <Card width="" height="100%" borderRadius="s" backgroundColor={theme.colors.white}>
+            <div css={styles.container}>
+                <ReportHeader testTheme={databaseInfo.name} author={userInfo.userName ?? ""} score={score} />
+                <Section title={"Questions"} >
+                    <div css={styles.sectionInnerContainer}>
+                        {testArray.map((testItem, index) => {
+                            return (
+                                <QuestionResultItem key={index} description={testItem.description} name={testItem.name} selectedOption={selectedOptions[index]} />
+                            )
+                        })}
+                    </div>
+                </Section>
+            </div>
+        </Card>
     )
 }
 
 const getStyles = (theme: Theme) => {
     return ({
         container: css({
-            backgroundColor: "pink"
-        })
+            padding: theme.spacing[6],
+            display: "flex",
+            flexDirection: "column",
+            gap: theme.spacing[6]
+        }),
+        sectionInnerContainer: css({
+            display: "flex",
+            flexDirection: "column",
+            gap: theme.spacing[6]
+        }),
     })
 }
 
